1e319e3b35c1a5a9f598744ab505256488d3889d
user-personal <suwantaraputra1@gmail.com>
2025-05-29 20:28:54 +0800

    update widget

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,8 +6,8 @@
     </x-slot>
 
     <div class="container py-4">
-        {{-- Widget Total Pesan --}}
         <div class="row g-4">
+            {{-- Widget Total Pesan --}}
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
@@ -23,6 +23,46 @@
                     <div class="card-body">
                         <h5 class="card-title">Pesan Baru</h5>
                         <p class="card-text h2">{{ \App\Models\Message::whereNull('read_at')->count() }}</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Widget Total Produk --}}
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Produk</h5>
+                        <p class="card-text h2">{{ \App\Models\Product::count() }}</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Widget Total Blog --}}
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Post</h5>
+                        <p class="card-text h2">{{ \App\Models\Post::count() }}</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Widget Total Kategori --}}
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Kategori</h5>
+                        <p class="card-text h2">{{ \App\Models\Category::count() }}</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Widget Total User --}}
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total User</h5>
+                        <p class="card-text h2">{{ \App\Models\User::count() }}</p>
                     </div>
                 </div>
             </div>
