e42d18eaefb0b74b19fc755166893e5818fe2f67
user-personal <suwantaraputra1@gmail.com>
2025-06-04 21:47:03 +0800

    update package

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top px-4 px-lg-5">
-    <a href="index.html" class="navbar-brand d-flex align-items-center">
+    <a href="{{ route('home') }}" class="navbar-brand d-flex align-items-center">
         <h1 class="m-0">Beratan Dairy</h1>
     </a>
     <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
